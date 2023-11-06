5da9a904ef2f7b306ad6fa6a7bb2ebdb14538020
jujusoi <jalxmcdonald@hotmail.com>
2023-11-06 13:54:19 +1030

    s

---
+++ client/src/utils/queries.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_SINGLE_PROFILE = gql`
+    query singleUser($userId: ID!) {
+        user(userId: $userId) {
+            _id
+            username: String
+            email: String
+            password: String
+            savedBooks: [String]!
+        }
+    }
+`;
